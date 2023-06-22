11fde86275e7a88dc7e566b086a53e2a022476ce
dbaron5 <131029961+dbaron5@users.noreply.github.com>
2023-06-22 10:04:08 -0400

    updates, still need team button to disappear

--- src/App.js
+++ src/App.js
@@ -14,19 +14,63 @@ function App() {
     { name: "justin", img: justin },
   ];
   const [students, setStudents] = useState(studentData);
+  const [bcArray, setBcArray] = useState([]);
+  const [binaryArray, setBinaryArray] = useState([]);
+
+  //constant to remove students created.
+
+  const removeFromStudents = (studentToRemove, targetArray) => {
+    if (students.includes(studentToRemove) === true) {
+      setStudents((prevStudents) =>
+        prevStudents.filter((student) => student.name !== studentToRemove.name)
+      );
+    }
+
+    //checks to see if teams include object.  Filter creates new array, and only students other than studentToRemove are included in filtered array.
+
+    if (bcArray.includes(studentToRemove) === true) {
+      setBcArray((prevStudents) =>
+        prevStudents.filter((student) => student.name !== studentToRemove.name)
+      );
+    }
+
+    if (binaryArray.includes(studentToRemove) === true) {
+      setBinaryArray((prevStudents) =>
+        prevStudents.filter((student) => student.name !== studentToRemove.name)
+      );
+    }
+
+    //if target=bc/binary, setBCArray will callback previous state of that array, new array with studentToRemove is moved to new array.
+
+    if (targetArray === "bcArray") {
+      setBcArray((prevBcArray) => [...prevBcArray, studentToRemove]);
+    } else if (targetArray === "binaryArray") {
+      setBinaryArray((prevBinaryArray) => [
+        ...prevBinaryArray,
+        studentToRemove,
+      ]);
+    }
+  };
+
+  const resetArrays = () => {
+    setStudents(studentData);
+    setBcArray([]);
+    setBinaryArray([]);
+  };
+
   return (
     <div className="topcontainer">
       <h1>Student Team React Challenge</h1>
-      <button
-        className="button-24"
-        onClick={() =>
-          console.log("I should reset you back to all being in default")
-        }
-      >
+      <button className="button-reset" onClick={resetArrays}>
         Reset
       </button>
       <div className="App">
-        <MainContainer students={students} />
+        <MainContainer
+          students={students}
+          bcArray={bcArray}
+          binaryArray={binaryArray}
+          removeFromStudents={removeFromStudents}
+        />
       </div>
     </div>
   );
